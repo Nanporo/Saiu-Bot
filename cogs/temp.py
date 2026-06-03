45527def8e029ce16a6680db05fae5eb747b665e
Kuuchi <kuuchi@twerg.org>
2026-06-03 16:02:52 +0800

    Update footer icon URL in temp.py

--- cogs/temp.py
+++ cogs/temp.py
@@ -74,7 +74,7 @@ class TempView(discord.ui.View):
         
         embed.description = "\n".join(lines)
         current_time = datetime.now(timezone(timedelta(hours=8))).strftime("%m-%d %H:%M")
-        embed.set_footer(text=f"中央氣象署 • 查詢時間 {current_time}", icon_url="https://raw.githubusercontent.com/Nanporo/TWERG-Bot/main/photos/cwa_logo.png")
+        embed.set_footer(text=f"中央氣象署 • 查詢時間 {current_time}", icon_url="https://raw.githubusercontent.com/Nanporo/Saiu-Bot/main/photos/cwa_logo.png")
 
         if self.show_image and self.image_url:
             embed.set_image(url=self.image_url)
