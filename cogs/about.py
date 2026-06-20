b5c750ec09feade1fa2e2f7c4dea0c4508d93266
Kuuchi <iamrogerwu0321@gmail.com>
2026-06-20 20:27:08 +0800

    update 1.6.2

--- cogs/about.py
+++ cogs/about.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 class AboutCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.version = "1.6.1"
+        self.version = "1.6.2"
         self.ready_printed = False
 
     @commands.Cog.listener()
