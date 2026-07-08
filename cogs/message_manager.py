f46506b4dc06f774d83222f0ec47a0819268817c
Kuuchi <iamrogerwu0321@gmail.com>
2026-07-08 22:29:01 +0800

    Update message_manager.py

--- cogs/message_manager.py
+++ cogs/message_manager.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import logging
+
+logger = logging.getLogger(__name__)
 
 class MessageManager(commands.Cog):
     def __init__(self, bot):
